af3001a3caa5e860b1c94bc2d90fa4815d98dca3
manuel <manuelmaiorano01@gmail.com>
2025-05-05 17:15:35 +0200

    stuff

---
+++ scripts/components/enemy_behaviour_component.gd
@@ -0,0 +1,70 @@
+extends Node
+
+@onready var state_chart: StateChart = %StateChart
+@onready var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") * ProjectSettings.get_setting("physics/3d/default_gravity_vector")
+
+@export var agent: CharacterBody3D
+@export var model: Node3D
+@export var animation_tree: AnimationTree
+@export var detection_area: Area3D
+
+@export var distance_to_attack: float
+
+var target: Node3D
+
+func _ready() -> void:
+	detection_area.body_entered.connect(_on_detection_area_body_entered)
+	
+func _on_detection_area_body_entered(body: Node3D) -> void:
+	if body == self:
+		return
+	target = body
+	state_chart.send_event("go_to_target")
+
+#IDLE
+func _on_idle_state_entered() -> void:
+	agent.animation_tree["parameters/Transition/transition_request"] = "idle"
+
+func _on_idle_state_physics_processing(delta: float) -> void:
+	agent.velocity.x = 0
+	agent.velocity.z = 0
+	agent.velocity += gravity * delta
+	
+	agent.move_and_slide()
+
+
+#WALK
+func _on_walk_state_entered() -> void:
+	agent.animation_tree["parameters/Transition/transition_request"] = "walk"
+
+func _on_walk_state_physics_processing(delta: float) -> void:
+	
+	if agent.global_position.distance_to(target.global_position) < distance_to_attack:
+		state_chart.send_event("attack")
+		return
+	
+	agent.rotate_towards_target(delta, target)
+	agent.apply_root_motion_to_velocity(delta)
+	agent.velocity += gravity * delta
+	
+	agent.move_and_slide()
+	agent.apply_orientation_to_model()
+
+
+
+#ATTACK
+func _on_attack_state_entered() -> void:
+	agent.animation_tree["parameters/Transition/transition_request"] = "attack"
+
+
+func _on_attack_state_physics_processing(delta: float) -> void:
+	
+	agent.apply_root_motion_to_velocity(delta)
+	agent.velocity += gravity * delta
+	
+	agent.move_and_slide()
+	agent.apply_orientation_to_model()
+
+
+func _on_attack_state_exited() -> void:
+	agent.animation_tree["parameters/Transition/transition_request"] = "idle"
---
+++ scripts/entity.gd
@@ -0,0 +1,40 @@
+extends CharacterBody3D
+@export var rotation_interpolate_spped = 10
+
+@export var animation_tree: AnimationTree
+@export var model: Node3D
+
+
+var orientation = Transform3D()
+var root_motion = Transform3D()
+
+
+func _ready():
+	# Pre-initialize orientation transform.
+	orientation = model.global_transform
+	orientation.origin = Vector3()
+	
+func rotate_towards_target(delta, target):
+	var direction = global_position - target.global_position
+	direction.y = 0
+	
+	var q_from = orientation.basis.get_rotation_quaternion()
+	var q_to = Transform3D().looking_at(-direction.normalized(), Vector3.UP, true).basis.get_rotation_quaternion()
+	
+	
+	# Interpolate current rotation with desired one.
+	orientation.basis = Basis(q_from.slerp(q_to, delta * rotation_interpolate_spped))
+
+func apply_root_motion_to_velocity(delta):
+	root_motion = Transform3D(animation_tree.get_root_motion_rotation(), animation_tree.get_root_motion_position())
+
+	orientation *= root_motion
+	
+	var h_velocity = orientation.origin / delta
+	velocity.x = h_velocity.x
+	velocity.z = h_velocity.z
+	orientation.origin = Vector3()
+
+func apply_orientation_to_model():
+	orientation = orientation.orthonormalized()
+	model.global_transform.basis = orientation.basis
