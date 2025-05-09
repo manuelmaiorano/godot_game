d1e830e33b9695ae4c9182186249e9f02893e19c
manuel <manuelmaiorano01@gmail.com>
2025-05-09 09:50:19 +0200

    stuff

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -4,10 +4,11 @@ const INVENTORY: ButtonGroup = preload("res://resources/buttons/inventory.tres")
 var item_list: Array
 
 var current_idx = 0
-@export var shop_items: Dictionary[Item, int]
+@onready var inventory: PanelContainer = %Inventory
+@onready var shop_inventory: PanelContainer = %ShopInventory
 
 @onready var cross_hair: CenterContainer = %CrossHair
-
+@export var shop_item_amounts: Dictionary[Item, int]
 
 
 func _ready() -> void:
@@ -17,6 +18,7 @@ func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	SignalBus.InventoryItemSelected.emit(item_list[current_idx].item)
 	SignalBus.ItemsChanged.connect(on_items_changed)
+	shop_inventory.set_items(shop_item_amounts)
 
 func on_items_changed(items: Dictionary[Item, int]):
 	for item in items.keys():
--- scripts/ui/shop_inventory.gd
+++ scripts/ui/shop_inventory.gd
@@ -1,3 +1,17 @@
 extends Control
 
 @export var who: Node
+@onready var inventory: GridContainer = %Inventory
+
+
+func set_items(items: Dictionary[Item, int]):
+	var idx = 0
+	for item in items.keys():
+		var amount = items[item]
+		var shop_item = inventory.get_child(idx) as ShopItem
+		shop_item.set_item(item)
+		shop_item.set_amount(amount)
+		idx += 1
+
+func get_selected():
+	pass
